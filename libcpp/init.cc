945f05d3f4076571eacd853980e2c51188588d83
Hannes Domani <ssbssa@yahoo.de>
2021-08-10 17:35:52 +0200

    Create switch to control redefined macro warning (PR preprocessor/83773)

--- libcpp/init.cc
+++ libcpp/init.cc
@@ -247,6 +247,7 @@ cpp_create_reader (enum c_lang lang, cpp_hash_table *table,
   CPP_OPTION (pfile, warn_dollars) = 1;
   CPP_OPTION (pfile, warn_variadic_macros) = 1;
   CPP_OPTION (pfile, warn_builtin_macro_redefined) = 1;
+  CPP_OPTION (pfile, warn_macro_redefined) = 1;
   CPP_OPTION (pfile, cpp_warn_implicit_fallthrough) = 0;
   CPP_OPTION (pfile, warn_header_guard) = 0;
   /* By default, track locations of tokens resulting from macro
